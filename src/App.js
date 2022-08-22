e771a2bebe12e387ac6cb4b90a915d3cc0b46fea
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-22 16:53:32 -0700

    added a expand class and border radius

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ function App() {
 
   return (
     <div className="App">
-      <motion.div layout onClick={() => setIsOpen(!isOpen)} className='card'>
-        <motion.h2>Framer Motion 🚀</motion.h2>
-        {isOpen && 
-        <motion.div>
+      <motion.div 
+      transition={{ layout: { duration: 1, type: "spring" } }} 
+      layout 
+      onClick={() => setIsOpen(!isOpen)} 
+      className="card"
+      style={{ borderRadius: '1rem'}}
+      >
+        <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+        {isOpen && (
+        <motion.div className="expand">
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
           Soluta officiis, in est saepe voluptates obcaecati illum 
           totam ducimus possimus? Vitae. 
@@ -17,8 +23,9 @@ function App() {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Aut, nihil.
           </p>
+          <button>Submit</button>
         </motion.div>
-        }
+        )}
       </motion.div>
     </div>
  );
